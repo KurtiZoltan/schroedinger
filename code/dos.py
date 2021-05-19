083be4ba5fe515d66251bc0e203aef3ce0ea68b3
Kürti Zoltán <kuzo1998@gmail.com>
2021-05-19 23:50:57 +0200

    Inf slope

--- code/dos.py
+++ code/dos.py
@@ -5,8 +5,8 @@ from plot import *
 def semiclassical(E):
     F = test.F
     L = test.L
-    n1 = 1/np.pi * 2 / 3 / F * (np.power(E, 3/2))
-    n2 = 1/np.pi * 2 / 3 / F * (np.power(E, 3/2) - np.power(np.maximum(E - F*L, 0), 3/2))
+    n1 = 1/np.pi * 2 / 3 / F * (np.power(E, 3/2)) - 3/4
+    n2 = 1/np.pi * 2 / 3 / F * (np.power(E, 3/2) - np.power(np.maximum(E - F*L, 0), 3/2)) - 1
     n1mask = E < L*F
     return n1 * n1mask + n2 * (1-n1mask)
 
@@ -18,7 +18,7 @@ y = np.zeros(x.shape)
 for i, currentx in zip(range(len(x)), x):
     y[i] = np.sum(elevels < currentx)
 
-plt.figure(figsize=[5, 4])
+plt.figure(figsize=[4, 3])
 plt.plot(x, y, label = "Állapotszám")
 plt.plot(x, semiclassical(x), label = "Szemiklasszikus közelítés")
 plt.xlabel("$E$")
