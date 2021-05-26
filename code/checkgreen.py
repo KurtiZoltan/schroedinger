09b10e45186ccdcfd8788b7e4c8f42451660d776
Kürti Zoltán <kuzo1998@gmail.com>
2021-05-27 01:19:18 +0200

    Green rewrite 2

---
+++ code/checkgreen.py
@@ -0,0 +1,17 @@
+import numpy as np
+from schroedinger import *
+from plot import *
+
+test = d1schroedinger(L=10)
+
+E = 5
+
+x = np.linspace(0, test.L, 10000)
+y = test.L/2
+G = test.G(x,y,E)
+I = E*G+np.gradient(np.gradient(G, x[1]-x[0]), x[1]-x[0])-test.F*x*G
+plt.plot(x[np.abs(x-y)<0.01], I[np.abs(x-y)<0.01],"o")
+plt.grid()
+plt.show()
+
+print(np.sum(I)*(x[1]-x[0]))
--- code/green.py
+++ code/green.py
@@ -4,10 +4,9 @@ import plotly.graph_objects as go
 from schroedinger import *
 from plot import *
 
-test = d1schroedinger(F = 0.1)
+test = d1schroedinger(L=10)
 
-n = 5
-E = 3+1j
+E = 5
 '''
 x = np.linspace(0, test.L, 1000)
 y = np.linspace(0, test.L, n)
@@ -19,22 +18,35 @@ plt.show()
 '''
 
 x = np.linspace(0, test.L, 1000)
-y = np.linspace(0, test.L, 1000)
-x, y = np.meshgrid(x, y)
-Galt = test.Galt(x, y, E)
-G = test.G(x, y, E)
+ys = test.L*np.array([0.2,0.4,0.6,0.8])
 
-plt.imshow(np.abs(G), aspect="equal", origin="lower", extent=(0, test.L, 0, test.L))
-plt.colorbar()
+plt.figure(figsize=(4,3))
+for y in ys:
+    plt.plot(x, test.G(x, y, E), label=f"$ay={y:.1f}$")
+plt.xlabel("ax")
+plt.ylabel("$F/a^2G(x,y,E)$")
+plt.grid()
+plt.legend()
+plt.savefig("../figs/1dgreens.pdf")
 plt.show()
-plt.imshow(np.abs(Galt), aspect="equal", origin="lower", extent=(0, test.L, 0, test.L))
-plt.colorbar()
-plt.show()
-'''
+
+# plt.imshow(np.abs(G), aspect="equal", origin="lower", extent=(0, test.L, 0, test.L))
+# plt.colorbar()
+# plt.show()
+# plt.imshow(np.abs(Galt), aspect="equal", origin="lower", extent=(0, test.L, 0, test.L))
+# plt.colorbar()
+# plt.show()
+# plt.imshow(G-Galt, aspect="equal", origin="lower", extent=(0, test.L, 0, test.L))
+# plt.colorbar()
+# plt.show()
+x = np.linspace(0, test.L, 500)
+y = np.linspace(0, test.L, 500)
+x, y = np.meshgrid(x, y)
+G = test.G(x,y,E)
 fig = go.Figure(data=[go.Surface(x=x, y=y, z=np.real(G))])
 fig.update_layout(scene = dict(
-    xaxis_title=r"x",
-    yaxis_title=r"y",
-    zaxis_title=r"G(x, y; E)"))
+    xaxis_title=r"ax",
+    yaxis_title=r"ay",
+    zaxis_title=r"G(x, y; E)*F/a^2"))
+fig.update_traces(showscale=False)
 fig.show()
-'''
